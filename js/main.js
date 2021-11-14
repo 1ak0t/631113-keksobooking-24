268bbc438dc146a2fa04d80e4d703e8a21a138f8
Игорь Кот <igorkot@MacBook-Kot.local>
2021-11-14 12:01:02 +0700

    Функции перевода страницы в активное и не активное состояние

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import {templateClone} from './create_ad.js';
 import './validation_module.js';
+import {disablePage,activatePage} from './page_state.js';
 
 document.querySelector('#map-canvas').appendChild(templateClone);
+disablePage();
+activatePage();
